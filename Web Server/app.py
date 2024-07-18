46d12d8a449cefa32453442e9932db4128d7bf08
yaq <yaq147@gmail.com>
2024-07-18 20:29:58 +0800

    I can tell which button the user presses

--- Web Server/app.py
+++ Web Server/app.py
@@ -6,7 +6,7 @@ import csv
 app = Flask(__name__)
 
 
-@app.route("/" , methods=["get","POST"])
+@app.route("/")
 def index():
     # Check if data.csv exists
     data = []
@@ -16,18 +16,28 @@ def index():
 
         for row in reader:
             data.append(row)
-
-
-    if request.method == 'POST':
-        for item in data:
-            #print (item)
-            #print(item["Name"])
-            if  request.form['addingToCart'] == item["Name"]:
-                print('works')
-                namee=item['Name']
-                writeC = writeCart(namee)
+    # if request.method == 'POST':
+    #     for item in data:
+    #         #print (item)
+    #         #print(item["Name"])
+    #         if  request.form['addingToCart'] == item["Name"]:
+    #             print('works')
+    #             namee=item['Name']
+    #             writeC = writeCart(namee)
     return render_template("main.html", data=data)
 
+
+### This is a test
+
+
+@app.route('/', methods=['POST'])
+def add_to_cart():
+    print("Request method:", request.method)
+    if 'addingToCart' in request.form:
+        buttonValue = request.form['addingToCart']
+        print("Button Value:",buttonValue)
+    return redirect(url_for('index'))
+###
 
 def writeCart(name):
     name = name 
@@ -42,24 +52,13 @@ def np():
     return render_template("productDatabase.html")
 
 
+
+
 # meant for adding items to cart
-
-
 @app.route("/cart")
 def cart():
     cart_items = session.get("cart", [])
     return render_template("cart.html", cart_items=cart_items)
-
-
-@app.route("/template")
-def template():
-    return render_template("template.html")
-
-
-@app.route("/filter")
-def filter():
-    return render_template("filteredPage.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
