da77e55cc676f7561a83ba33f8e30f61b7737872
yaq <yaq147@gmail.com>
2024-08-15 20:26:37 +0800

    runn all at once

--- runtime.py
+++ runtime.py
@@ -1,27 +1,58 @@
-import InsightAddon
-import insightTracker
-import LCDoutput
-import ultrasound
-import appSite
+import multiprocessing
+import subprocess
+import threading
 
 
-def run_the_scripts():
-    #Runs the website
-    appSite.run_appSite()
-    #Runs the LCD & keypad
-    LCDoutput.run_LCDoutput()
-    
-    #runs ultrasound then Tracker , Addon
-    ultrasound.run_ultrasound()
-    insightTracker.run_insightTracker()
-    InsightAddon.run_InsightAddon()
-    
-    print('DONE?')
-    
+def run_website():
+    # runs the website first
+    subprocess.Popen('python3 appSite.py', shell=True)
+    print('DONE website?')
+
+
+def run_LCD():
+    subprocess.Popen('python3 LCDoutput.py', shell=True)
+    print('Done LCD & keypad')
+
+
+def run_Ultrasound():
+    subprocess.Popen('python3 ultrasound.py', shell=True)
+    print('Done Ultrasound')
+
+
+def run_Insight_Tracker():
+    subprocess.Popen('python3 insightTracker.py', shell=True)
+    print('Done Insight Tracker')
+
+
 if __name__ == "__main__":
     while True:
         try:
-            run_the_scripts()
+            multiPro = []
+            # starts the website
+            website_process = multiprocessing.Process(target=run_website)
+            multiPro.append(website_process)
+            # Website doesnt turn off
+            # for the LCD & Keypad
+            LCD_process = multiprocessing.Process(target=run_LCD)
+            # LCD also doesnt turn off automatically
+            multiPro.append(LCD_process)
+            for i in multiPro:
+                i.start()
+
+            ##########################################################################################
+            threads = []
+            Ultrasound_thread = threading.Thread(target=run_Ultrasound)
+            threads.append(Ultrasound_thread)
+            Insight_thread = threading.Thread(target=run_Insight_Tracker)
+            threads.append(Insight_thread)
+            for thr in threads:
+                thr.start()
         except KeyboardInterrupt:
-            print('NOt doing it')
-        finally: break
+            print('NOt doing it!!!')
+            for i in multiPro:
+                i.join()
+            for i in threads:
+                i.join()
+            # stops the processes
+        finally:
+            break
